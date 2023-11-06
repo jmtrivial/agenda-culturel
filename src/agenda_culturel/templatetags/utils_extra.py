bfdb9c634267ff61671ab66ad474461cc8c9b26c
Jean-Marie Favreau <j-marie.favreau@uca.fr>
2023-11-06 08:40:30 +0100

    Ajout d'un lien pour la journée

--- src/agenda_culturel/templatetags/utils_extra.py
+++ src/agenda_culturel/templatetags/utils_extra.py
@@ -3,6 +3,7 @@ from django.utils.safestring import mark_safe
 
 from urllib.parse import urlparse
 from datetime import timedelta, date
+from django.urls import reverse_lazy
 
 register = template.Library()
 
@@ -17,21 +18,26 @@ def hostname(url):
 def add_de(txt):
     return ("d'" if txt[0].lower() in ['a', 'e', 'i', 'o', 'u', 'y'] else "de ") + txt
 
+
 @register.filter
 def week(d):
     return d.isocalendar()[1]
+
 
 @register.filter
 def shift_day(d, shift):
     return d + timedelta(days=shift)
 
+
 @register.filter
 def first_day_of_this_week(d):
     return date.fromisocalendar(d.year, week(d), 1)
 
+
 @register.filter
 def last_day_of_this_week(d):
     return date.fromisocalendar(d.year, week(d), 7)    
+
 
 @register.filter
 def calendar_classes(d, fixed_style):
@@ -44,3 +50,8 @@ def calendar_classes(d, fixed_style):
     if not d.on_requested_interval:
         result += " other_month"
     return result
+
+
+@register.filter
+def url_day(d):
+    return reverse_lazy("day_view", kwargs={"year": d.year, "month": d.month, "day": d.day})
