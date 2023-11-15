02966f478efde57b3ada8557bb940456c47fd08c
Jean-Marie Favreau <jeanmarie.favreau@free.fr>
2023-11-15 17:25:33 +0100

    s'il y a la catégorie vide on créé quand même les autres

--- src/scripts/create_categories.py
+++ src/scripts/create_categories.py
@@ -17,7 +17,7 @@ def run():
         ("Autre", "Autres événements", "A")
     ]
 
-    if len(Category.objects.all()) == 0:
+    if len(Category.objects.all()) <= 1:
         print("On créée des catégories")
         for c in categories:
             cat = Category(name=c[0], alt_name = c[1] if c[1] is not None else c[0], codename=c[2])
