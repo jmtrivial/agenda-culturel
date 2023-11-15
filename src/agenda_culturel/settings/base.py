015d33e142991e3e20e4a3d4bd4dbf4ccb523c65
Jean-Marie Favreau <j-marie.favreau@uca.fr>
2023-11-15 23:12:43 +0100

    ajout d'une adresse d'expédition

--- src/agenda_culturel/settings/base.py
+++ src/agenda_culturel/settings/base.py
@@ -189,3 +189,4 @@ if os_getenv("EMAIL_BACKEND"):
     EMAIL_HOST_PASSWORD = os_getenv("EMAIL_HOST_PASSWORD")
     EMAIL_USE_TLS = os_getenv("EMAIL_USE_TLS", False)
     EMAIL_USE_SSL = os_getenv("EMAIL_USE_SSL", False)
+    DEFAULT_FROM_EMAIL = os_getenv("DEFAULT_FROM_EMAIL")
